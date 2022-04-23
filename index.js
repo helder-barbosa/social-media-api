ae795887ef18aed000c4debe531ee61e7037f9ac
Helder Barbosa <helderbarbosa.dev@gmail.com>
2022-04-22 23:25:05 -0300

    #2 Routes / Login and Register

--- index.js
+++ index.js
@@ -4,10 +4,21 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const port = process.env.PORT || 3001;
+const userRoute = require('./routes/users');
+const authRoute = require('./routes/auth');
+require('dotenv').config();
 
-mongoose.connect(`${process.env.MONGO_URL}`, () => {
-  console.log('[DATABASE] Connected to MongoDB !');
-});
+mongoose
+  .connect(`${process.env.MONGO_URL}`)
+  .then(() => console.log('[DATABASE] Connected to MongoDB !'))
+  .catch((e) => console.log(e));
+
+app.use(express.json());
+app.use(helmet());
+app.use(morgan('common'));
+
+app.use('/api/users', userRoute);
+app.use('/api/auth', authRoute);
 
 app.listen(port, () => {
   console.log('[SERVER] Server Running...');
